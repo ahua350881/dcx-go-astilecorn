4879dc8029d6091529e60b3d459050d9c3982250
chenchengjie <1097639834@qq.com>
2021-08-09 20:18:17 +0800

    electron download url update

--- paths.go
+++ paths.go
@@ -143,10 +143,10 @@ func ElectronDownloadSrc(os, arch, versionElectron string) string {
 	} else if strings.ToLower(arch) == "arm64" {
 		a = "arm64"
 	}
-
+	return fmt.Sprintf(" https://byedu-print.oss-cn-shenzhen.aliyuncs.com/electron/release/download/v11.4.3/electron-windows-amd64-v11.4.3.zip")
 	// https://byedu-print.oss-cn-shenzhen.aliyuncs.com/electron/release/download/v11.4.3/electron-windows-amd64-v11.4.3.zip
 	// Return url
-	return fmt.Sprintf("https://byedu-print.oss-cn-shenzhen.aliyuncs.com/electron/releases/download/v%s/electron-v%s-%s-%s.zip", versionElectron, versionElectron, o, a)
+	//return fmt.Sprintf("https://byedu-print.oss-cn-shenzhen.aliyuncs.com/electron/releases/download/v%s/electron-v%s-%s-%s.zip", versionElectron, versionElectron, o, a)
 }
 
 // initAppExecutable initializes the app executable path
